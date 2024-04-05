31181859c02a361444e069018bb4b67b343aafd8
Aman vishwakarma <aman.vns27@gmail.com>
2024-04-06 00:18:38 +0530

    chat page added

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,29 @@
 import React from "react";
-import Chats from "./Chats"
-const GeneralApp = () => {
+import { Stack, Box } from "@mui/material";
+import Chats from "./Chats";
+import Conversation from "../../components/Conversation";
+import { useTheme } from "@mui/material/styles";
 
+const GeneralApp = () => {
+  const theme = useTheme();
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
       {/* Chats*/}
-    <Chats/>
-    </>
+      <Chats />
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        {/*onversation */}
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
