a7964ed41beed079171fdbf96d88cdd130502aa7
Aryaman Verma <aryamanskate01@gmail.com>
2024-04-13 12:55:01 +0530

    Call page Added

--- src/routes/index.js
+++ src/routes/index.js
@@ -39,6 +39,7 @@ export default function Router() {
         { path: "app", element: <GeneralApp /> },
         { path: "settings", element: <Settings /> },
         { path: "group", element: <GroupPage /> },
+        { path: "call", element: <CallPage /> },
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
@@ -59,6 +60,7 @@ const NewPasswordPage = Loadable(lazy(()=>import("../pages/auth/NewPassword")));
 const Settings = Loadable(
   lazy(() => import("../pages/dashboard/Settings")),
 );
+const CallPage=Loadable(lazy(()=>import("../pages/dashboard/Call")));
 const GroupPage = Loadable(
   lazy(() => import("../pages/dashboard/Group")),
 );
