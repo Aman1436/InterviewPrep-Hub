c61933089141d40bab005f87be2c420c42bdffe3
Aditya Chaturvedi <achatur755@gmail.com>
2024-04-14 19:33:14 +0530

    User registartion and login added with otp verification

--- chat-server/controllers/auth.js
+++ chat-server/controllers/auth.js
@@ -1,40 +1,161 @@
 //for using jsonwebtoken
-const jwt=require("jsonwebtoken");
-
-
-
+const jwt = require("jsonwebtoken");
+//for otp generation
+const otpGenerator = require("otp-generator");
 
 //Now performing the CRUD operations
 
-const User=require("../models/user");
+const User = require("../models/user");
 
 //defining the sign token function
-const signToken=(userId)=>jwt.sign({userId},process.env.JWT_SECRET);
+const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
 
+//Register New User
+//collecting the data during registaration from the body
+exports.register = async (req, res, next) => {
+  const { firstName, lastName, email, password, verified } = req.body;
 
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "email",
+    "password"
+  );
 
+  // check if a verified user with given email address
+  //We will be sending otp for verification
 
+  //If user with the same email address already exists,we will throw a error message
+  const existing_user = await User.findOne({ email: email });
+  //if user already exists and already done verification using otp
+  if (existing_user && existing_user.verified) {
+    return res
+      .status(400)
+      .json({
+        status: "error",
+        message: "User with this email already exists.Please login.",
+      });
+  }
+  //if user already exists and not done verification using otp
+  else if (existing_user) {
+    const updated_user = await User.findOneAndUpdate(
+      { email: email },
+      filteredBody,
+      { new: true, validateModifiedOnly: true }
+    );
 
-exports.login = async (req, res,next) => {
-   //getting the email and password from the request body
+    //pass the control to next middleware
+    req.userId = existing_user._id;
+    next();
+  } else {
+    //if user record is not found in the database
+    //We need to create new user record
+    const new_user = await User.create(filteredBody);
 
-   const { email, password } = req.body;
+    //generate the otp and send email to the user
+    req.userId = new_user._id;
+    next();
+  }
+};
 
-   if(!email || !password){
-       return res.status(400).json({status :'error',message:"Both email and password are required"});
-   }
-   //Finding the user in the database from the username and password provided
-   const userDoc = await User.findOne({ email: email }).select("+password");
+//for sending the otp
+exports.sendOTP = async (req, res, next) => {
+  const { userId } = req;
+  //used the npm package named otp-generator
+  const new_otp = otpGenerator.generate(6, {
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
 
-   //If user not found
-   if(!userDoc||!(await userDoc.correctPassword(password,userDoc.password))){
-       return res.status(400).json({status :'error',message:"Email  or password is incorrect"});
-   }
+  //now we need to set the time for expiry of the otp(10 minutes from the time of otp generation)
+  const otp_expiry_time = Date.now() + 10 * 60 * 1000;
 
-   //If user and password matched
-   const token = signToken(userDoc._id);
-   res.status(200).json({status :'success',message:"Logged in successfully",data: { token }});
- 
+  await User.findbyIdOneAndUpdate(userId, { otp: new_otp, otp_expiry_time });
 
+  //TODO Sending the email to the user
 
-}
+  res.status(200).json({ status: "success", message: "OTP sent successfully" });
+};
+
+//For verifying the otp and update the user record accordingly
+exports.verifyOTP = async (req, res, next) => {
+  const { email, otp } = req.body;
+
+  const user = await User.findOne({
+    email: email,
+    otp_expiry_time: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    res
+      .status(400)
+      .json({
+        status: "error",
+        message: "Email is invalid or OTP has expired",
+      });
+  }
+
+  //We will also store the otp in the DB in the hash format
+  //Then we need to compare the otps
+
+  //If userOTP is incorrect
+  if (!(await user.correctOTP(otp, user.otp))) {
+    res.status(400).json({ status: "error", message: "OTP is incorrect" });
+  }
+
+  //If userOTP is correct
+
+  user.verified = true;
+  user.otp = undefined;
+
+  await user.save({ new: true, validateModifiedOnly: true });
+
+  const token = signToken(user._id);
+  res
+    .status(200)
+    .json({ status: "success", message: "OTP verified successfully", token });
+};
+
+exports.login = async (req, res, next) => {
+  //getting the email and password from the request body
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({
+        status: "error",
+        message: "Both email and password are required",
+      });
+  }
+  //Finding the user in the database from the username and password provided
+  const userDoc = await User.findOne({ email: email }).select("+password");
+
+  //If user not found
+  if (
+    !userDoc ||
+    !(await userDoc.correctPassword(password, userDoc.password))
+  ) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "Email  or password is incorrect" });
+  }
+
+  //If user and password matched
+  const token = signToken(userDoc._id);
+  res
+    .status(200)
+    .json({
+      status: "success",
+      message: "Logged in successfully",
+      data: { token },
+    });
+};
+
+//For resetting the password
+exports.forgotPassword = async (req, res, next) => {};
+
+exports.resetPassword = async (req, res, next) => {};
--- chat-server/models/user.js
+++ chat-server/models/user.js
@@ -46,14 +46,47 @@ const userSchema = new mongoose.Schema({
   updatedAt: {
     type: Date,
   },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  otp: {
+     type:Number,
+  },
+  otp_expiry_time:{
+     type:Date,
+  }
 });
 
+userSchema.pre("save", async function (next) {
+   //We need to run theis function if OTP is actually modified 
+   if(!this.isModified("otp")){
+      return next();   
+   }
+
+
+   // Hashing the otp with the cost of 12
+   this.otp=await bcrypt.hash(this.otp,12);
+
+   next();
+})
+
+
+//for comparing the password entered and that in the DB in the encrypted format
 userSchema.methods.correctPassword = async function (
   candidatePassword,//password suppied by the user
   userPassword//password stored in the database
 ){
   return await bcrypt.compare(candidatePassword,userPassword)
 }
+
+//for comparing the otp entered and that in the DB in the encrypted format
+userSchema.methods.correctOTP = async function (
+   candidateOTP,//password suppied by the user
+   userOTP//password stored in the database
+ ){
+   return await bcrypt.compare(candidateOTP,userOTP)
+ }
 
 
 
